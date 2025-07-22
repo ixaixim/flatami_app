87e2533c028173e4157a034f9bebfb40164242f1
Neel Misciasci <83877391+ixaixim@users.noreply.github.com>
2025-07-22 18:14:23 +0200

    Add placeholder pages and router

---
+++ client/src/pages/LookForFlatPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function LookForFlatPage() {
+  return (
+    <div>
+      <h1>Look for Flat</h1>
+    </div>
+  );
+}
+
+export default LookForFlatPage;
---
+++ client/src/pages/MainPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function MainPage() {
+  return (
+    <div>
+      <h1>Main Screen</h1>
+    </div>
+  );
+}
+
+export default MainPage;
